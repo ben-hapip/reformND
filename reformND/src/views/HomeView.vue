0e2927370e19663283567747c143e17c954a3c76
Ben Hapip <benhapipcodes@gmail.com>
2023-09-02 18:22:19 -0500

    REFORMED

---
+++ reformND/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="page" style="width:100%"><img src="../components/icons/ndLegendary.jpg"/> </div>
+</template>
---
+++ reformND/src/views/PastConferences.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page">
+    <h1 class="header">Agenda Conference</h1>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/716221730297943/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/716221733284675/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/71622211301680/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/716222220372602/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+    <iframe tabindex="-1" width="100%" height="150" src="https://embed.sermonaudio.com/player/a/59192240101052/" style="min-width: 150px;" frameborder="0" scrolling="no"></iframe>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/71622222514684/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+    <h1 class="header">Local Church Reformation Conference</h1>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/103122155759680/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/103122155915331/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/1031221559256757/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/10312216011406/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+    <div style="position:relative;width:100%;height:0;padding-bottom:56.25%"><iframe tabindex="-1" width="100%" height="100%" src="https://embed.sermonaudio.com/player/v/103122160332624/" style="position:absolute;left:0;top:0" allowfullscreen frameborder="0" scrolling="no"></iframe></div>
+  </div>
+</template>
+
+<style scoped>
+.header{
+  text-decoration: underline;
+   margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+</style>
